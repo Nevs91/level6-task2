14826e4e81ab7e86b108854ba957de8935d7b978
Sven van Putten <svenvanputten@gmail.com>
2020-11-30 16:04:07 +0100

    Moved the MainActivity and fragment classes to a new 'ui' package

--- app/src/main/java/com/example/madlevel6task2/ui/PopularMoviesFragment.kt
+++ app/src/main/java/com/example/madlevel6task2/ui/PopularMoviesFragment.kt
@@ -1,22 +1,23 @@
-package com.example.madlevel6task2
+package com.example.madlevel6task2.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.example.madlevel6task2.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class MovieDetailFragment : Fragment() {
+class PopularMoviesFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        return inflater.inflate(R.layout.fragment_popular_movies, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
